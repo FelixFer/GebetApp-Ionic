46d53a2690dadb13f818348ed628f22c8559a1c6
felix <felixf020401@gmail.com>
2022-03-07 15:38:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FelixFerdinand_00000035927_IF670_AL_UTS',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
